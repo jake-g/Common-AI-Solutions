ea214e78461ed7c718f35c484ae63b93a922a8bc
Jake Garrison <omonoid@uw.edu>
2016-10-30 03:26:35 -0700

    started methods

--- ai.py
+++ ai.py
@@ -1,75 +1,83 @@
 import time
-import random 
+import random
 import io
+
 
 class key:
     def key(self):
         return "10jifn2eonvgp1o2ornfdlf-1230"
+
 
 class ai:
     def __init__(self):
         pass
 
     class state:
-        def __init__(self, a, b, a_fin, b_fin):
+        def __init__(self, a, b, af, bf, parent=None):
             self.a = a
             self.b = b
-            self.a_fin = a_fin
-            self.b_fin = b_fin
+            self.af = af
+            self.bf = bf
+            self.parent = None
 
-    # Kalah:
-    #         b[5]  b[4]  b[3]  b[2]  b[1]  b[0]
-    # b_fin                                         a_fin
-    #         a[0]  a[1]  a[2]  a[3]  a[4]  a[5]
-    # Main function call:
-    # Input:
-    # a: a[5] array storing the stones in your holes
-    # b: b[5] array storing the stones in opponent's holes
-    # a_fin: Your scoring hole (Kalah)
-    # b_fin: Opponent's scoring hole (Kalah)
-    # t: search time limit (ms)
-    # a always moves first
-    #
-    # Return:
-    # You should return a value 0-5 number indicating your move, with search time limitation given as parameter
-    # If you are eligible for a second move, just neglect. The framework will call this function again
-    # You need to design your heuristics.
-    # You must use minimax search with alpha-beta pruning as the basic algorithm
-    # use timer to limit search, for example:
-    # start = time.time()
-    # end = time.time()
-    # elapsed_time = end - start
-    # if elapsed_time * 1000 >= t:
-    #    return result immediately 
-    def move(self, a, b, a_fin, b_fin, t):
-        #For test only: return a random move
-        r = []
-        for i in range(6):
-            if a[i] != 0:
+        def __str__(self):
+            return 'a: %r (%r)\nb: %r (%r)\n' \
+                   % (self.a, self.af, self.b, self.bf)
+
+    def random_move(self):
+        r = []  # return a random move
+        for i in range(6):  # Nonempty moves
+            if self.state.a[i] != 0:
                 r.append(i)
-        # To test the execution time, use time and file modules
-        # In your experiments, you can try different depth, for example:
-        f = open('time.txt', 'a') #append to time.txt so that you can see running time for all moves.
-        # Make sure to clean the file before each of your experiment
-        for d in [3, 5, 7]: #You should try more
-            f.write('depth = '+str(d)+'\n')
-            t_start = time.time()
-            self.minimax(depth = d)
-            f.write(str(time.time()-t_start)+'\n')
+        return r[random.randint(0, len(r) - 1)]
+
+    def get_states(self):
+        def get_moves(player):
+            for i, n in enumerate(player):
+                if n > 0:
+                    yield i  # ith hole not empty
+
+        def apply_move(move, player=1):
+            new = self.state
+            if player:  # 1 = player a
+                beads = self.state.a[move]
+                # FINISH THIS!!!!!!!!!
+                if beads > move:
+                    for i in beads % 14
+                    new.a = [n+1 for n in self.state.a]
+                    new.af += 1
+
+                    print 'score!'
+
+            return new
+
+
+
+
+        for move in get_moves(self.state.a):
+            new_state = apply_move(move)
+            yield new_state
+
+    def move(self, a, b, af, bf, t):
+        self.state = ai.state(a, b, af, bf)
+        d = 3  # depth
+        f = open('fuck.txt', 'a')  # Make sure to clean the file before each of your experiment
+        f.write('Move: depth = ' + str(d) + '\n')
+        t_start = time.time()
+        print 'time', t
+
+        move = self.minimax(d)
+
+        f.write(str(time.time() - t_start) + '\n')
         f.close()
-        return r[random.randint(0, len(r)-1)]
-        #But remember in your final version you should choose only one depth according to your CPU speed (TA's is 3.4GHz)
-        #and remove timing code. 
-        
-        #Comment all the code above and start your code here
+        print 'move %r' % move
+        return move
 
     # calling function
     def minimax(self, depth):
-        #example: doing nothing but wait 0.1*depth sec
-        time.sleep(0.1*depth)
-
-
-
-
-
-
+        print self.state
+        for state in self.get_states():
+            print state
+        print '-----'
+        time.sleep(0.1 * depth)
+        return self.random_move()
