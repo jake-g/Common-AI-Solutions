7eaab57cc9380e6bafeea9f6657a3dc2bc21132b
Jake Garrison <omonoid@uw.edu>
2016-10-30 22:43:46 -0700

    cleaned

--- ai.py
+++ ai.py
@@ -25,7 +25,7 @@ class ai:
             self.last_move = None
 
         def __str__(self):
-            return 'last move: %r\na: %r (%r)\nb: %r (%r)\n' \
+            return 'move: %r\na: %r (%r)\nb: %r (%r)\n' \
                    % (self.last_move, self.a, self.af, self.b, self.bf)
 
     def random_move(self):
@@ -35,34 +35,51 @@ class ai:
                 r.append(i)
         return r[random.randint(0, len(r) - 1)]
 
-    def get_states(self):
-        def get_moves(player):
+    def get_states(self, max_player):
+        def get_moves():
+            if max_player:
+                player = self.state.a
+            else:
+                player = self.state.b
             for i, n in enumerate(player):
                 if n > 0: yield i  # ith hole not empty
 
         def new_state(move):
             # returns new state resulting in applied move
-
             assert move < 6
             assert self.state.a[move] > 0
 
+            def apply_move(move, board):
+                beads = board[move]
+                board[move] = 0  # grab beads
+                while beads >= 0:
+                    move += 1
+                    board[move % 13] += 1  # drop bead
+                    beads -= 1
+                return board
+
             new = copy.deepcopy(self.state)  # clone game state
             new.last_move = move
-            board = new.a + [new.af] + new.b
-            beads = board[move]
-            board[move] = 0 # grab beads
-            while beads >= 0:
-                move += 1
-                board[move % 13] += 1  # drop bead
-                beads -= 1
 
-            new.a = board[0:6]
-            new.af = board[6]
-            new.b = board[7::]
+            if max_player:  # a is moving
+                board = new.a + [new.af] + new.b
+                board = apply_move(move, board)
+                new.a = board[0:6]
+                new.af = board[6]
+                new.b = board[7::]
+            else:    # b is moving
+                board = new.b + [new.bf] + new.a
+                board = apply_move(move, board)
+                new.b = board[0:6]
+                new.bf = board[6]
+                new.a = board[7::]
+
             return new
 
-        for move in get_moves(self.state.a):
-            yield new_state(move)  # new state
+
+        for move in get_moves():
+            yield new_state(move )  # new state
+
 
     def move(self, a, b, af, bf, t):
         self.state = ai.state(a, b, af, bf)
@@ -72,17 +89,33 @@ class ai:
         t_start = time.time()
         print 'time', t
 
-        move = self.minimax(d)
+        next_state = self.minimax(d)
 
         print '-----'
         f.write(str(time.time() - t_start) + '\n')
         f.close()
         # print 'move %r' % move
-        return move
+        return next_state.last_move
 
     # calling function
-    def minimax(self, depth):
-        for state in self.get_states():
-            print state
-        time.sleep(0.1 * depth)
-        return self.random_move()
+    def minimax(self, depth, max_player=True, state=None):
+        if depth == 0: # or out of moves
+            print 'reached depth'
+            return
+
+        if max_player:
+            best_val = -999
+            for state in self.get_states(max_player):
+                child = self.minimax(depth-1, not max_player, state)
+                if child.af > best_val:
+                    best_val = child.af
+                    best_state = child
+
+        else :
+            best_val = 999
+            for state in self.get_states(max_player):
+                child = self.minimax(depth-1, not max_player, state)
+                if child.af < best_val:
+                    best_val = child.af
+                    best_state = child
+        return best_state
